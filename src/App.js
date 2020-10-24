b0c384f670bf0d94de2f386eb38cfa17f49323be
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-10-24 15:38:04 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+import React ,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  render(){
+    return <div>
+      <h1>React</h1>
+      <p>This is sample compenent.</p>
+      <p>これはサンプルのコンポーネントです。</p>
+    </div>
+  }
+}
+
+export default App;
