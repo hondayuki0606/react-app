be7ab35c635cd07f612eea97f920c24e4d72548c
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-10-28 23:06:55 +0900

    コンポーネントを組み合わせる リスト3-13 リスト3-14

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 //import React from 'react';
-import React ,{Component} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 
 class App extends Component {
-  constructor(props){
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
+
   render(){
     return <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
+      <h1>React</h1>
+      <p><Rect x="50" y="50" w="150" h="150" c="cyan" /></p>
+      <p><Rect x="150" y="150" w="150" h="150" c="black" /></p>
     </div>
   }
 }
+class Rect extends Component {
+  x = 0;
+  y = 0;
+  witdh = 0;
+  height = 0;
+  color = "black"
+  style = {};
+  constructor(props){
+    super(props);
+    this.x = props.x;
+    this.y = props.y;
+    this.witdh = props.w;
+    this.height = props.h;
+    this.color = props.c;
+    this.style = {
+      backgroundColor:this.color,
+      position:"absolute",
+      left:this.x + "px",
+      top:this.y + "px",
+      witdh:this.witdh + "px",
+      height:this.height + "px",
+    }
+  }
+  render(){
+    return <div style={this.style}>tete</div>
+  }
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App title="App2" message="This is App Component"/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
