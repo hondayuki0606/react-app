9ef3c5374877fd69aecb72c360412cef40d9dacb
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-10-29 21:21:23 +0900

    Rectコンポーネントを利用する P141

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import React from 'react';
 import React, {Component} from 'react';
+import Rect from './Rect';
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
@@ -9,38 +10,11 @@ class App extends Component {
   render(){
     return <div>
       <h1>React</h1>
-      <p><Rect x="50" y="50" w="150" h="150" c="cyan" /></p>
-      <p><Rect x="150" y="150" w="150" h="150" c="black" /></p>
-    </div>
+      <p><Rect x="50" y="50" w="150" h="150" c="cyan" r="50"/></p>
+      <p><Rect x="150" y="100" w="150" h="222" c="black" r="75"/></p>
+      <p><Rect x="100" y="150" w="150" h="150" c="magenta" r="25"/></p>
+    </div>;
   }
 }
-class Rect extends Component {
-  x = 0;
-  y = 0;
-  witdh = 0;
-  height = 0;
-  color = "black"
-  style = {};
-  constructor(props){
-    super(props);
-    this.x = props.x;
-    this.y = props.y;
-    this.witdh = props.w;
-    this.height = props.h;
-    this.color = props.c;
-    this.style = {
-      backgroundColor:this.color,
-      position:"absolute",
-      left:this.x + "px",
-      top:this.y + "px",
-      witdh:this.witdh + "px",
-      height:this.height + "px",
-    }
-  }
-  render(){
-    return <div style={this.style}>tete</div>
-  }
-}
-
 
 export default App;
