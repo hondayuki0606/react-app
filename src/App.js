76166b2e554da75c7761fab825871daa8e9a094a
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-10-31 01:53:22 +0900

    ステートを用意する P146

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 //import React from 'react';
 import React, {Component} from 'react';
 import Rect from './Rect';
-import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
 
 class App extends Component {
 
+  msgStyle = {
+    fontSize:"20pt",
+    color:"#900",
+    margin:"20px",
+  }
+  constructor(props){
+    super(props);
+    this.state = {
+      msg:'Hello Component.',
+    };
+  }
   render(){
     return <div>
       <h1>React</h1>
-      <p><Rect x="50" y="50" w="150" h="150" c="cyan" r="50"/></p>
-      <p><Rect x="150" y="100" w="150" h="222" c="black" r="75"/></p>
-      <p><Rect x="100" y="150" w="150" h="150" c="magenta" r="25"/></p>
+      <p style={this.msgStyle}>{this.state.msg}</p>
+      <p style={this.msgStyle}>{this.props.msg}</p>
     </div>;
   }
 }
