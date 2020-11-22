84ef034762b343b76e71b35db9484badbdcea85d
Firstname Lastname <hondayuki0606@yahoo.co.jp>
2020-11-23 00:54:53 +0900

    onSubmitとpreventDefault P171

--- src/App.js
+++ src/App.js
@@ -10,63 +10,50 @@ class App extends Component {
     fontSize:"20pt",
     color:"#900",
     margin:"20px 0px",
+    padding:"5px",
+  }
+  inputStyle = {
+    fontSize:"20pt",
     padding:"5px"
   }
+
   constructor(props){
     super(props);
     this.state = {
       message:'type your name:'
     };
-    this.doChange = this.dochange = this.doChange.bind(this);
-    this.doSubmit = this.dochange = this.doSubmit.bind(this); 
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this); 
   }
 
    doChange(event) {
      this.input = event.target.value;
    }
 
-   doSubmit(event) {
+   doSubmit(event){
      this.setState({
-       messaga: 'Hello, ' + this.input + '!!'
+       message: 'Hello, ' + this.input + '!!'
      });
+     // 下記をコメントアウトするとページが再ロードされてしまう
+     // このメソッドはイベントを消費するメソッドです。
+     // 消費というのは、これで発生したイベントをなくすという意味です。
+     // イベントをなくすと、もう実際にフォームが送信されることはなくなります。
      event.preventDefault();
    }
+   
    render(){
     return <div>
       <h1>React</h1>
-      <Message title="Children!">
-        これはコンポーネント内のコンテンツです。
-        まるでテキストを分割し、リストにして表示します。
-        改行はありません。
-      </Message>
+      <h2>{this.state.message}</h2>
+      <form onSubmit={this.doSubmit}>
+       <label>
+        <span style={this.inputStyle}></span>Message:
+         <input type="text" style={this.inputStyle}
+         onChange={this.doChange} />
+       </label>
+       <input type="submit" style={this.inputStyle} value="Click" />
+      </form>
     </div>;
-  }
-}
-
-class Message extends Component {
-
-  li = {
-    fontSize:"16pt",
-  }
-  render(){
-    let content = this.props.children;
-    let arr = content.split('。');
-    let arr2 = [];
-    for(let i = 0; i < arr.length;i++){
-      if(arr[i].trim() != ''){
-        arr2.push(arr[i]);
-      }
-    } 
-    let list = arr2.map((value,key)=>(
-      <li style={this.li} key={key}>{value}.</li>)
-    );
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <p style={this.title}>{this.props.title}</p>
-        <o>{list}</o>
-      </div>
-    )
   }
 }
 export default App;
